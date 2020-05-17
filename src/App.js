dea5b1d18dc30819a2291b981f9e800e2f408f6b
teo23 <matteoazzola23@gmail.com>
2020-05-17 19:52:47 +0200

    added toggle theme option

--- src/App.js
+++ src/App.js
@@ -1,36 +1,4 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// App.js
-
-
-// App.js
-
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import './App.css';
 import Home from './Components/Home';
 import About from './Components/About';
@@ -39,9 +7,40 @@ import {projects as Projects} from './Components/Projects';
 import ProjectDetails from './Components/ProjectDetails';
 import { Switch, Route } from 'react-router-dom';
 
-class App extends Component {
-  render() {
+import { ThemeProvider, createGlobalStyle } from 'styled-components';
+import storage from 'local-storage-fallback';
+import style from 'styled-theming';
+import Button from './Components/Button';
+
+const getBackground = style('mode' , {
+  light: 'EEE',
+  dark: '#111'
+});
+
+const getForeground = style('mode' , {
+  light: 'EEE',
+  dark: '#111'
+});
+
+const GlobalStyle = createGlobalStyle`
+body {
+  background-color: ${getBackground};
+  color: ${getForeground};
+}
+`;
+
+function getInitialTheme() {
+  const savedTheme = storage.getItem('theme')
+  return savedTheme ?JSON.parse(savedTheme) : { mode: 'light'}
+}
+
+
+export default function App() {
+    const [theme, setTheme] = useState(getInitialTheme)
     return (
+      <ThemeProvider theme= {theme}>
+      <>
+      <GlobalStyle/>
       <div className="App">
         <Navbar />
         <Switch>
@@ -49,10 +48,45 @@ class App extends Component {
           <Route path='/about' component={About}/>
           <Route exact path='/projects' component={Projects}/>
           <Route exact path="/projects/:id" component={ProjectDetails} />
-        </Switch>
+        </Switch> 
+        <button
+            onClick = {e =>
+            setTheme(
+              theme.mode === 'dark'
+              ? { mode: 'light' }
+              : { mode: 'dark' }
+            )
+            }
+          >
+          Toggle Theme
+          </button>
       </div>
+      </>
+      </ThemeProvider>
     );
-  }
 }
 
-export default App;
+
+// class App extends Component {
+//   render() {
+//     const [theme, setTheme] = useState({ mode: 'light'})
+//     return (
+//       <ThemeProvider theme= {theme}>
+//       <>
+//       <GlobalStyle/>
+//       <div className="App">
+//         <Navbar />
+//         <Switch>
+//           <Route exact path='/' component={Home}/>
+//           <Route path='/about' component={About}/>
+//           <Route exact path='/projects' component={Projects}/>
+//           <Route exact path="/projects/:id" component={ProjectDetails} />
+//         </Switch> 
+//       </div>
+//       </>
+//       </ThemeProvider>
+//     );
+//   }
+// }
+
+// export default App;
